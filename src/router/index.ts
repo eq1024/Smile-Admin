dd655aa9afd0c8ddbd291be6aefecfb0523b4168
vigo <vigo@xwork.cn>
2023-09-06 17:02:46 +0800

    hash模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import type { RouteRecordRaw } from 'vue-router'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Layout from '@/layout/index.vue'
 
 const {
@@ -142,8 +142,8 @@ export const routes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  // history: createWebHashHistory(),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [dashboardRoute, ...routes, ...constantRoutes],
 })
 
